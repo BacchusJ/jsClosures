1071af46f21d0537511811fee824f9e22b9b97e1
Vesper Jones <vesperjones@Vespers-MacBook-Pro.local>
2019-07-04 00:36:52 -0500

    retirement calculator and interview questions using functions

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function retirement(retiremenetAge) {
     var a = ' years left until retirement';
     return function(yearOfBirth) {
-        var age = 2016 - yearOfBirth;
+        var age = 2019 - yearOfBirth;
         console.log((retiremenetAge - age) + a);
 
     }
@@ -10,7 +10,27 @@ function retirement(retiremenetAge) {
 //create one function for each country 
 
 var retiremenentUS = retirement(66);
-retiremenentUS(1977);
+var retiremenetGermany = retirement(65);
+var retiremenetIceland = retirement(67);
 
-//we can also use 
-retirement(66)(1977);
+retiremenentUS(1977);
+retiremenetGermany(1977);
+retiremenetIceland(1977);
+
+
+//we can also use and get the same result
+// retirement(66)(1977);
+
+function interviewQuestion(job) {
+    return function(name) {
+        if (job === 'teacher') {
+            console.log(name + ', tell us what do you teach? ');
+        } else if (job === 'drag queen') {
+            console.log(name + ', do you lipsync for your life?');
+        } else {
+            console.log('Tell us more about what you do');
+        }
+    }
+}
+
+interviewQuestion('drag queen')('Jennifer');
